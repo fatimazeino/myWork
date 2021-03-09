5629fda7b74b357413e39b5d3ec94c6305a34d1a
unknown <fatemazeno.fz@gmail.com>
2021-03-09 08:12:25 +0000

    lab week02

---
+++ week02/nameAndAge.py
@@ -0,0 +1,7 @@
+# helloWorld.py
+# This program t reads in a name and age and outputs print outs them
+# Autgor: Fatima Zeino
+
+name = input('Enter your name: ')
+age = input('Enter your age: ')
+print('Hello {},\t your age is {}.'.format(name,age))
