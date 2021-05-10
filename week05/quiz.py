8f146103a26fa577d63ce39bdffb7862d0a2e63c
unknown <fatemazeno.fz@gmail.com>
2021-05-10 02:06:07 +0100

    week05 labs

---
+++ week05/quiz.py
@@ -0,0 +1,10 @@
+# quiz.py
+# Author: Fatima Zeino
+
+# function yo takes one parameter
+def yo(a):
+ return a * 2
+# here we are calling the function yo
+# and are passing in an argument, 3
+y=yo(3)
+print (y)
